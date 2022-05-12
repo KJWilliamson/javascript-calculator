5d31df11fc333c642e62bce9d533da1901016130
kamela williamson <kami63116@yahoo.com>
2022-05-11 20:09:31 -0700

    changed colors on design

---
+++ script.js
@@ -0,0 +1,9 @@
+// When the user clicks a number or operator button, the display should update with the value of the button
+// When the clear button is clicked, the display should reset/clear
+// When the equals button is clicked, the display should update with the result of the equation that is displayed
+
+// Update the display when a number or operator button click
+// need to get all the buttons and their values from the DOM
+// need to add an event listener to handle the clicks
+// updating the display, so we'll need to get the display from the DOM as well
+
